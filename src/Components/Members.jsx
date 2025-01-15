c2665c116953df141b3b0bbaf60d5f28ac765a2d
chrixob <chrisobodai40@gmail.com>
2025-01-15 14:09:25 +0000

    added a new image and created a new component

---
+++ src/Components/Members.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+// import { useState } from "react";
+import president from "../images/executives/president.jpg"
+import vice from "../images/executives/vice.jpg"
+import organiser from "../images/executives/organiser.jpg"
+import deputyorganiser from "../images/executives/deputyorganiser.jpg"
+import secretary from "../images/executives/secretary.jpg"
+import financialsec from "../images/executives/financialsec.jpg"
+
+
+
+function Members(){
+
+    return(
+        <>
+        </>
+    );
+}
